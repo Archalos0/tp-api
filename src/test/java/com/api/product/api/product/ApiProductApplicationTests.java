c833975a318c7e17a4f9f7ab2dec123d53b99695
Archalos0 <73042136+Archalos0@users.noreply.github.com>
2022-02-24 11:50:42 +0100

    init avec Spring Initializr

---
+++ src/test/java/com/api/product/api/product/ApiProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.product.api.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
